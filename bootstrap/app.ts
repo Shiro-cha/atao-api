50a2f6ebe4e905aa8f302cb9aac6bdb2d5295940
Shiro-cha <noum.rzdr@gmail.com>
2023-04-10 03:35:09 +0300

    Message by stp

--- bootstrap/app.ts
+++ bootstrap/app.ts
@@ -1,11 +1,14 @@
 import express,{Express} from "express";
 import {json,urlencoded} from "body-parser";
+import cors from "cors";
 import Routes from "../app/providers/routesProvider";
 
 
 const app:Express = express();
 
 //Définir ici les middleware personnalisé
+
+app.use(cors);
 
 //display all request
 app.use(function(req,res,next){
